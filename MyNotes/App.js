f4788f3d0274487797f6c542eaa51f1da2283198
rpasricha45 <rpasricha45@gmail.com>
2018-11-03 21:35:50 -0400

    - update

--- MyNotes/App.js
+++ MyNotes/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SearchBar, Header, Card } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <View style = {styles.container}>
+            <View style = {styles.testHeader}>
+
+            <Header
+                centerComponent={{text: 'What Up Bitch', style:{color: '#fff'}}}
+               />
+
+
+            
+
+            </View>
+            <View style = {styles.searchbar}>
+        <SearchBar
+        placeholder='Type Here...'/>
+            </View>
+       </View>
     );
   }
 }
@@ -15,7 +29,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    //justifyContent: 'space-between',
   },
+   searchbar: {
+     // bottom:-100,
+     alignSelf:'stretch',
+     alignItems:'stretch',
+
+     flex:1,
+
+
+  },
+  testHeader: {
+      // justifyContent: 'flex-start',
+      // bottom:100,
+      flex:1,
+      alignSelf:'stretch',
+
+
+  }
+
+
+
+
 });
